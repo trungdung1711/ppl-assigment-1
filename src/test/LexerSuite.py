40315f6e117f979e0cbf9c31832730db9a6cddd9
zun <trungdunglebui17112004@gmail.com>
2025-02-12 18:39:46 +0700

    dung-partially done the lexer rule, able to be tested again the lexer suite, able to test unrecognized token error and also unclosed string token error

--- src/test/LexerSuite.py
+++ src/test/LexerSuite.py
@@ -8,9 +8,20 @@ class LexerSuite(unittest.TestCase):
     
     def test_wrong_token(self):
         self.assertTrue(TestLexer.checkLexeme("ab?sVN","ab,ErrorToken ?",102))
+    
     def test_keyword_var(self):
         """test keyword var"""
         self.assertTrue(TestLexer.checkLexeme("var abc int ;","var,abc,int,;,<EOF>",103))
+    
     def test_keyword_func(self):
         """test keyword func"""
         self.assertTrue(TestLexer.checkLexeme("""func abc ( ) ""","""func,abc,(,),<EOF>""",104))
+    
+    def test_single_line_comment(self):
+        self.assertTrue(TestLexer.checkLexeme("""//comment\n""","""<EOF>""",105))
+
+    def test_unrecognized_character(self):
+        self.assertTrue(TestLexer.checkLexeme("""$value""","""ErrorToken $""",106))
+    
+    def test_unclosed_string(self):
+        self.assertTrue(TestLexer.checkLexeme(""""this is a string""","""Unclosed string: this is a string""",107))
