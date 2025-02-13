d6f4a47b9f0add3e3f940887a62b7a99612ab8b9
zun <trungdunglebui17112004@gmail.com>
2025-02-13 21:52:49 +0700

    dung-add tests for lexer

--- src/test/LexerSuite.py
+++ src/test/LexerSuite.py
@@ -32,4 +32,66 @@ class LexerSuite(unittest.TestCase):
     def test_replacing_semicolon_2(self):
         self.assertTrue(TestLexer.checkLexeme("""a := 100-200+500/2.3e12\n""","""a,:=,100,-,200,+,500,/,2.3e12,;,<EOF>""",110))
 
-#       self.assertTrue(TestLexer.checkLexeme(""" """,""" """,110))
+    def test_white_space_being_skipped(self):
+        self.assertTrue(TestLexer.checkLexeme(""" \t\f\r\n \t\f\r\n""","""<EOF>""",111))
+
+    def test_single_line_comment_2(self):
+        self.assertTrue(TestLexer.checkLexeme("""// \"This is\\ the\\ comment **537%$3^%%$#$^%\nfor""","""for,<EOF>""",112))
+
+    def test_simple_multipline_comment(self):
+        self.assertTrue(TestLexer.checkLexeme("""/*This is a\n multiple\n comment*/float""","""float,<EOF>""",113))
+
+    def test_nested_multi_line_comment(self):
+        self.assertTrue(TestLexer.checkLexeme("""/*/*nested*/comment/*nested/*nested*/*/*/if""","""if,<EOF>""",114))
+
+    def test_nested_multi_line_comment_with_open_comment(self):
+        self.assertTrue(TestLexer.checkLexeme("""/*com/*ment/*ne/*sted*/*/boolean""","""boolean,<EOF>""",115))
+
+    def test_very_complex_comment_and_multi_line_comment(self):
+        self.assertTrue(TestLexer.checkLexeme("""
+        /*/*comment*/com/*comment*//*comment*//*comment*/ment/*comment*/*/
+        /*/*comment*/co/*comment*//*comment*//*comment*/mment/*comment*/*/
+        /*comment*//*comment*//*comment*/
+        /*comment*//*comment*//*comment*/
+        // comment// comment// comment// 
+        // comment
+        // comment
+        // comment
+        // comment
+        func""","""func,<EOF>""",116))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,117))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,118))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,119))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,120))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,121))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,122))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,123))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,124))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,125))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,126))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,127))
+
+    def test_(self):
+        self.assertTrue(TestLexer.checkLexeme(""" """,""" """,128))
